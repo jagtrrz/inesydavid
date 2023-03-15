de2af7a268ccf1f5db8310da17ad8c69dc516546
jagtrrz <jagutierrezc7@gmail.com>
2023-03-15 21:56:20 +0100

    feat: update gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,69 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return new Promise((resolve, reject) => {
     resolve(
-      graphql().then(result => {
+      graphql(
+        `
+          query {
+            services: allMarkdownRemark(
+              filter: { fileAbsolutePath: { regex: "content/services\/.*/" } }
+              sort: { fields: [frontmatter___date], order: DESC }
+            ) {
+              edges {
+                node {
+                  id
+                  excerpt
+                  frontmatter {
+                    title
+                    date(formatString: "DD MMMM YYYY")
+                  }
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+            team: allMarkdownRemark(
+              filter: { fileAbsolutePath: { regex: "content/team\/.*/" } }
+              sort: { fields: [frontmatter___date], order: DESC }
+            ) {
+              edges {
+                node {
+                  id
+                  excerpt
+                  frontmatter {
+                    title
+                    promoted
+                    image
+                    date(formatString: "DD MMMM YYYY")
+                  }
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+            basic: allMarkdownRemark(
+              filter: { fileAbsolutePath: { regex: "content/basic\/.*/" } }
+            ) {
+              edges {
+                node {
+                  id
+                  excerpt
+                  html
+                  frontmatter {
+                    title
+                    path
+                    template
+                  }
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        `,
+      ).then(result => {
         resolve();
       }),
     );
