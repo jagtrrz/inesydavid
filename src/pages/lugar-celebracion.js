19d6b9c672ef7b13819f0ae618618193e757d54f
Jose Angel Gutiérrez <jagutierrezc7@gmail.com>
2023-03-15 21:28:20 +0100

    Dev main (#6)
    * Cleaning (#2)
    
    * recolocamos textos y secciones
    
    * adapto form a la version mobile
    
    * quito imagenes
    
    * añadimos goolge maps
    
    * ajutamos responsives, mentemos estilos y terminamos mapa
    
    * cambio el favicon y el title
    
    * creamos una vista por seccion
    
    * cambio rutas imagenes
    
    * fuerzo commit
    
    * fuerzo commit
    
    * Trigger rebuild
    
    * cambio pathprefix
    
    * cambio pathprefix
    
    * Trigger rebuild
    
    * quito mapa de prueba
    
    * feat: remove comment code
    
    * feat: update github actions
    
    * feat: update github actions
    
    * feat: reset changes
    
    * feat: update node version
    
    * feat: update node version
    
    * feat: update node version
    
    * feat: update gatsby config
    
    * feat: add console.log
    
    * feat: cleaning useless code
    
    * feat: update config
    
    * feat: remove useless code

--- src/pages/lugar-celebracion.js
+++ src/pages/lugar-celebracion.js
@@ -38,7 +38,6 @@ const Home = (props) => {
 export const query = graphql`
   query {
     services: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/services/.*/" } }
       sort: { fields: [frontmatter___weight], order: ASC }
       limit: 6
     ) {
@@ -54,16 +53,6 @@ export const query = graphql`
           }
           excerpt
         }
-      }
-    }
-    intro: markdownRemark(fileAbsolutePath: { regex: "/content/index.md/" }) {
-      html
-      frontmatter {
-        image
-        intro_image
-        intro_image_absolute
-        intro_image_hide_on_mobile
-        title
       }
     }
     features: allFeaturesJson {
